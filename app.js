77fa7f21a785788c595dd3d23721af870986e1ca
Pol Benedito Momplet <polbene@gmail.com>
2020-06-25 16:52:39 +0200

    package.json fail fixed

--- app.js
+++ app.js
@@ -87,6 +87,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
 app.use('/stories', require('./routes/stories'))
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
